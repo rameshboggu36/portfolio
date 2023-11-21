a32ab128056b9e331982c569a92b6620c7eccb8d
ramesh_b <86396489+rameshboggu36@users.noreply.github.com>
2023-11-21 12:32:12 +0530

    Added updated javascript file.

---
+++ script.js
@@ -0,0 +1,32 @@
+var tablinks = document.getElementsByClassName("tab-links");
+    var tabcontents = document.getElementsByClassName("tab-contents");
+
+    function opentab(tabname){
+        for(tablink of tablinks){
+            tablink.classList.remove("active-link");
+        }
+        for(tabcontent of tabcontents){
+            tabcontent.classList.remove("active-tab");
+        }
+        event.currentTarget.classList.add("active-link");
+        document.getElementById(tabname).classList.add("active-tab");
+    }
+
+
+
+    const scriptURL = 'https://script.google.com/macros/s/AKfycby_GiqGib4FNRAkekD6SNGZgUfJQkJv8ZDvLLxFgomKo9WBbHG1_Uu06ARhCieVvt_g/exec'
+    const form = document.forms['submit-to-google-sheet']
+    const msg = document.getElementById("msg")
+  
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => {
+            msg.innerHTML = "Message sent successfully"
+            setTimeout(function(){
+                msg.innerHTML = ""
+            },5000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+    })
